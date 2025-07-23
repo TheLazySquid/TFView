0c99172ef902587875e3e1371b5dfac9b6d3cf09
TheLazySquid <thelazysquid2@gmail.com>
2025-07-23 16:18:03 -0400

    Set up release workflow

--- backend/scripts/build.ts
+++ backend/scripts/build.ts
@@ -1,5 +1,6 @@
 import { $ } from "bun";
 import { cp, rmdir, exists } from "node:fs/promises";
+import { zip } from "zip-a-folder";
 
 if(await exists("dist")) {
     console.log("Clearing dist directory...");
@@ -9,17 +10,18 @@ if(await exists("dist")) {
 console.log("Building binary...");
 // Waiting for https://github.com/oven-sh/bun/pull/20530 to be merged before icon/hideconsole can be used
 // await $`bun build ./src/index.ts --compile --minify --bytecode --windows-icon=./resource/icon.ico --windows-hide-console --outfile dist/tfview`.text();
-await $`bun build ./src/index.ts --compile --minify --bytecode --outfile dist/tfview`.quiet();
+await $`bun build ./src/index.ts --compile --minify --bytecode --outfile dist/unpacked/tfview`.quiet();
 
 console.log("Copying static files...");
-await cp("static", "dist/static", { recursive: true });
+await cp("static", "dist/unpacked/static", { recursive: true });
 
 console.log("Copying README.md...");
-await cp("../README.md", "dist/README.md");
-
-if(Bun.argv.includes("--zip")) {
-    console.log("Zipping the dist directory...");
-    await $`cd dist && 7z a -tzip tfview.zip ./*`.quiet();
+await cp("../README.md", "dist/unpacked/README.md");
+ 
+let zipIndex = Bun.argv.indexOf("--zip");
+if(zipIndex !== -1) {
+    let name = Bun.argv[zipIndex + 1] ?? "tfview";
+    await zip("./dist/unpacked", `./dist/${name}.zip`);
 }
 
 console.log("Done!");
